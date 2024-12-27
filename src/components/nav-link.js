64d1fc1aeeea19b78c481fbe36b14807e08e9a39
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-12-27 12:02:50 +0300

    use clsx library for conditionally adding css classes

--- src/components/nav-link.js
+++ src/components/nav-link.js
@@ -5,12 +5,19 @@ import {usePathname} from "next/navigation";
 
 import classes from "./nav-link.module.css"
 
+import clsx from "clsx";
+
 export default function NavLink({href, children}){
     const path = usePathname()
-    // console.log(path)
+    console.log(`path: ${path}`)
+    console.log(`href: ${href}`)
     return(
         <Link href={href}
-              className={path.startsWith(href) ? `${classes.link} ${classes.active}`: `${classes.link}`}
+              // className={path.startsWith(href) ? `${classes.link} ${classes.active}`: `${classes.link}`}
+            className={clsx(
+                classes.link,
+                { [classes.active]: path == href }
+            )}
         >{children}
         </Link>
 
